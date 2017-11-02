f3e73a950175911e8123d6f68250384335763cfd
Francisco Ortola Piles <frorpi@floridauniversitaria.es>
2017-11-02 10:35:24 +0100

    letra

--- prueba.js
+++ prueba.js
@@ -1,26 +1,28 @@
+var bucle=false;
+var intentos=0;
+var letra="";
+var letraUsuario="";
+
 numeroAleatorio=Math.floor(Math.random()*(96-123+1)+123);
 console.log(numeroAleatorio);
 
-var letra = String.fromCharCode(numeroAleatorio);
+letra = String.fromCharCode(numeroAleatorio);
 console.log(letra);
 
-var letraUsuario = prompt("Dime una letra");
+letraUsuario = prompt("Dime una letra");
 
 
 while (letraUsuario!= letraUsuario.toLowerCase()) {
   alert("La letra es mayuscula");
-  var letraUsuario = prompt("Escribe una letra minuscula");
+  letraUsuario = prompt("Escribe una letra minuscula");
 }
 
-var bucle=false;
-var intentos=0;
-
 while (letraUsuario!=letra && intentos<5) {
-  var letraUsuario = prompt("Fallaste, dime otra letra");
+  letraUsuario = prompt("Fallaste, dime otra letra");
   intentos++;
 
   if (letraUsuario==letra) {
     document.write("Acertaste");
-  var bucle=true;
+    bucle=true;
   }
 }
